7c1aa040c1243a9333dc0e25f652f052b2f9c0f7
stemadde <stemadde2@libero.it>
2022-07-26 11:56:18 +0200

    added crud TODOs

--- prova/crud.py
+++ prova/crud.py
@@ -1,5 +1,25 @@
+from typing import Optional
+
+from django.contrib.auth.models import User
+from django.db.models import QuerySet
 from prova.models import BlogPost
 
 
-def get_blog_posts(user):
+def get_blog_posts(user: User) -> QuerySet:
     return BlogPost.objects.filter(user=user)
+
+
+def create_blog_post(title: str, text: str, user: User, color: str) -> BlogPost:
+    pass
+
+
+def delete_blog_post(post_id: int) -> int:
+    """
+    :param post_id:
+    :return: Status code -> 200 = ok, 500 = error
+    """
+    pass
+
+
+def updated_blog_post(post_id: int, **kwargs) -> Optional[BlogPost]:
+    pass
--- prova/views.py
+++ prova/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from prova.crud import get_blog_posts
+from prova import crud
 
 
 def test_view(request):
     if request.user.is_authenticated:
+        blog_posts = crud.get_blog_posts(request.user)
         return render(request, 'prova/test.html', {
             'title': 'Pippo',
-            'blog_posts': get_blog_posts(request.user),
+            'blog_posts': blog_posts,
         })
     else:
         return HttpResponse('Accedi per visualizzare il contenuto')
